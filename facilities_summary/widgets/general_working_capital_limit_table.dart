224c40db5f31f9ce39e48f2b5078b6c51ff10c46
Abhijith VM <abhijith.vm@mphasis.com>
2025-06-30 23:13:21 +0530

    Update general_working_capital_limit_table.dart

--- facilities_summary/widgets/general_working_capital_limit_table.dart
+++ facilities_summary/widgets/general_working_capital_limit_table.dart
@@ -1,137 +1,107 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:wcas_frontend/core/components/accordion.dart';
+import 'package:wcas_frontend/core/components/button.dart';
 import 'package:wcas_frontend/core/components/custom_table/table.dart';
 import 'package:wcas_frontend/core/constants/constants.dart';
-import 'package:wcas_frontend/features/request/facilities_securities/facilities_summary/fields/applicable_pricing.dart';
 import 'package:wcas_frontend/features/request/facilities_securities/facilities_summary/fields/existing_limits.dart';
-import 'package:wcas_frontend/features/request/facilities_securities/facilities_summary/fields/margin.dart';
 import 'package:wcas_frontend/features/request/facilities_securities/facilities_summary/fields/outstanding.dart';
 import 'package:wcas_frontend/features/request/facilities_securities/facilities_summary/fields/proposed_limits.dart';
 import 'package:wcas_frontend/features/request/facilities_securities/facilities_summary/fields/sustanability_classification.dart';
-import 'package:wcas_frontend/features/request/facilities_securities/facilities_summary/fields/tenor_days.dart';
 import 'package:wcas_frontend/features/request/facilities_securities/facilities_summary/model.dart';
-import 'package:wcas_frontend/features/request/facilities_securities/facilities_summary/widgets/add_sublimit_button.dart';
 import 'package:wcas_frontend/models/request/facility_security/facility_summary.dart';
 
 class GeneralWorkingCapitalLimitTable extends StatelessWidget {
   final FacilitiesSummaryViewModel viewModel;
   final GeneralWorkingCapitalLimits generalWorkingCapitalLimits;
-  final int customerListIndex;
+  final CustomerFacility customer;
+
   const GeneralWorkingCapitalLimitTable({
     super.key,
     required this.viewModel,
     required this.generalWorkingCapitalLimits,
-    required this.customerListIndex,
+    required this.customer,
   });
 
   @override
   Widget build(BuildContext context) {
     return CustomAccordion(
-      title: "facilitySummary.generalWorking".tr(),
-      textColor: AppColors.primary,
+      title: "facilitySummary.generalWorkingCapital".tr(),
       children: [
+        const SizedBox(height: 20),
         CustomRawTable(
           columns: getTableColumns(),
           rows: getTableRows(),
-        )
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 14.0),
+          child: Row(
+            spacing: 20,
+            children: [
+              CustomButton(
+                label: "facilitySummary.addRow".tr(),
+                onPressed: () {
+                  // Handle add row
+                },
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
 
-  List<List<Widget>> getTableRows() {
-    List<List<Widget>> rows = [];
+  List<TableColumn> getTableColumns() {
+    return [
+      TableColumn(label: Text('facilitySummary.sNo'.tr())),
+      TableColumn(label: Text('facilitySummary.details'.tr())),
+      TableColumn(label: Text('facilitySummary.classification'.tr())),
+      TableColumn(label: Text('facilitySummary.existingLimits'.tr())),
+      TableColumn(label: Text('facilitySummary.proposedLimits'.tr())),
+      TableColumn(label: Text('facilitySummary.os'.tr())),
+      TableColumn(label: Text('facilitySummary.action'.tr())),
+    ];
+  }
 
-    for (int index = 0;
-        index < (generalWorkingCapitalLimits.facilities ?? []).length;
-        index++) {
-      rows.add([
-        AddSublimitButton(
-          serialNumber:
-              (generalWorkingCapitalLimits.facilities ?? [])[index].sNo,
-          viewModel: viewModel,
-          customerFacilityListIndex: index,
-        ),
-        Text(((generalWorkingCapitalLimits.facilities ?? [])[index]
-                .facilityDetails ??
-            "")),
+  List<List<Widget>> getTableRows() {
+    final facilities = generalWorkingCapitalLimits.facilities ?? [];
+    return List.generate(facilities.length, (index) {
+      final facility = facilities[index];
+      return [
+        Text((facility.sNo).toString()),
+        Text((facility.facilityDetails ?? "")),
         SustanabilityClassificationField(
           viewModel: viewModel,
-          sustanabilityClassification: generalWorkingCapitalLimits
-                  .facilities?[index].sustainabilityClassification ??
-              [],
+          sustanabilityClassification: facility.sustainabilityClassification ?? [],
         ),
         ExistingLimits(
           viewModel: viewModel,
-          customerListIndex: customerListIndex,
-          facilityListIndex: index,
+          customer: customer,
+          facility: facility,
           typeID: generalWorkingCapitalLimits.typeId,
         ),
         ProposedLimits(
           viewModel: viewModel,
+          customer: customer,
+          facility: facility,
           typeID: generalWorkingCapitalLimits.typeId,
-          facilityListIndex: index,
-          customerListIndex: customerListIndex,
         ),
         OutstandingAmount(
           viewModel: viewModel,
-          typeID: generalWorkingCapitalLimits.typeId,
-          facilityListIndex: index,
-          customerListIndex: customerListIndex,
-        ),
-        TenorDays(
-          viewModel: viewModel,
-          typeID: generalWorkingCapitalLimits.typeId,
-          facilityListIndex: index,
-          customerListIndex: customerListIndex,
-        ),
-        ApplicablePricing(
-          viewModel: viewModel,
-          typeID: generalWorkingCapitalLimits.typeId,
-          facilityListIndex: index,
-          customerListIndex: customerListIndex,
-        ),
-        Margin(
-          viewModel: viewModel,
-          customerListIndex: customerListIndex,
-          facilityListIndex: index,
+          customer: customer,
+          facility: facility,
           typeID: generalWorkingCapitalLimits.typeId,
         ),
-      ]);
-    }
-
-    return rows;
-  }
-
-  List<TableColumn> getTableColumns() {
-    return [
-      TableColumn(
-        label: Text('facilitySummary.sNo'.tr()),
-      ),
-      TableColumn(
-        label: Text('facilitySummary.details'.tr()),
-      ),
-      TableColumn(
-        label: Text('facilitySummary.classification'.tr()),
-      ),
-      TableColumn(
-        label: Text('facilitySummary.existingLimits'.tr()),
-      ),
-      TableColumn(
-        label: Text('facilitySummary.proposedLimits'.tr()),
-      ),
-      TableColumn(
-        label: Text('facilitySummary.os'.tr()),
-      ),
-      TableColumn(
-        label: Text('facilitySummary.tenorDays'.tr()),
-      ),
-      TableColumn(
-        label: Text('facilitySummary.applicablePricing'.tr()),
-      ),
-      TableColumn(
-        label: Text('facilitySummary.margin'.tr()),
-      ),
-    ];
+        IconButton(
+          icon: const Icon(Icons.delete, color: AppColors.overdueColor),
+          onPressed: () {
+            viewModel.deleteFacilityDetails(
+              serialNumber: facility.sNo,
+              typeID: generalWorkingCapitalLimits.typeId,
+            );
+          },
+        ),
+      ];
+    });
   }
 }
